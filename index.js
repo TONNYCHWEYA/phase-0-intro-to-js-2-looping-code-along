da514644a5e90b906efbf81753347bf06045ab5e
TONNYCHWEYA <chweyatonny@gmail.com>
2022-06-08 22:23:09 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solutions in this file
+const newArray =[];
+
+function writeCards(array, event){
+  
+  for(let i = 0; i <array.length; i++){
+    newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+  }
+  
+  return newArray;
+}
